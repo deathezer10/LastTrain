8cde79d19f9e25c150cc9cc87efea18659be7e6d
TSITCL\s162034 <elitesr2110@gmail.com>
2018-10-11 11:03:12 +0900

    ~ Add
    + CallTrain.Scene
    + CallButton.cs
    + GetClickObject.cs
    + Gizmo.cs
    
    ~ Setting
    
    * DOTween
    * SteamVR
    
    Tokiya

---
+++ Assets/Scripts/Tokiya/CallButtom.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CallButtom : MonoBehaviour
+{
+
+    private  bool _isCall = false;
+
+    private GameObject Button;
+
+    public GameObject Train;
+    public GameObject Station;
+
+    public float speed;
+    public float comeTime = 3.0f;
+    
+    void Update()
+    {
+        // Test 
+        comeTime -= Time.deltaTime;
+
+        if (comeTime < 0) {_isCall = true;}
+
+        if (_isCall)
+        {
+            ChangeColor();
+            ArriveTrain();
+            comeTime = 99999;
+        }
+
+        // If the button is pressed
+
+        /* ボタンを作る
+        
+        Colliderでコントローラーとボタンが接触していたら 
+        && 
+        コントローラーが押されていたら
+    
+         */
+
+    }
+
+    void ArriveTrain()
+    {
+        Train.transform.LookAt
+            (Station.transform.position);
+
+        Train.transform.DOMove
+            (Station.transform.position, speed).SetEase(Ease.OutExpo);
+
+        _isCall = false;
+    }
+
+    // オブジェクトの色を変える
+    void ChangeColor()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
---
+++ Assets/Scripts/Tokiya/GetClickObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetClickObject : MonoBehaviour
+{
+    private GameObject Test;
+
+
+    GameObject clickedGameObject;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                clickedGameObject = hit.collider.gameObject;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+}
