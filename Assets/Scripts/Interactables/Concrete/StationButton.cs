0529764ef7ce251c9c4a4b3d857694892c5d37b1
Hui Sheng @ Laptop <deathezer10@gmail.com>
2018-10-15 15:25:37 +0900

    Initial Station button

---
+++ Assets/Scripts/Interactables/Concrete/StationButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class StationButton : MonoBehaviour {
+
+    public GameObject m_Train;
+    const float m_TrainStoppingPoint = 7.4f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Material>().SetColor("_Color", Color.red);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GameController")
+        {
+            GetComponent<Material>().SetColor("_Color", Color.green);
+            m_Train.GetComponent<TrainArriver>().BeginArrival();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Train/TrainArriver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TrainArriver : MonoBehaviour {
+
+    public const float m_TrainStoppingPoint = 7.4f;
+
+    public void BeginArrival()
+    {
+        transform.DOMoveX(m_TrainStoppingPoint, 5);
+    }
+
+}
