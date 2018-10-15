e2d9bdae6d925836c5decaec7ae88beee8ae3854
Hui Sheng @ Laptop <deathezer10@gmail.com>
2018-10-15 17:26:24 +0900

    initial door opening after train reaches

--- Assets/Scripts/Train/TrainArriver.cs
+++ Assets/Scripts/Train/TrainArriver.cs
@@ -5,11 +5,15 @@ using DG.Tweening;
 
 public class TrainArriver : MonoBehaviour {
 
-    public const float m_TrainStoppingPoint = 4.85f;
+    public TrainDoorHandler m_TrainDoor;
+
+    const float m_TrainStoppingPoint = 4.85f;
 
     public void BeginArrival()
     {
-        transform.DOMoveZ(m_TrainStoppingPoint, 10).SetEase(Ease.OutQuart);
+        transform.DOMoveZ(m_TrainStoppingPoint, 10).SetEase(Ease.OutQuart).OnComplete(()=> {
+            m_TrainDoor.ToggleDoors(true);
+        });
     }
 
 }
