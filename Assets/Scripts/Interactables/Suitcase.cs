de89bc7a90c5f9f5d38d728f4cb731d008ee3564
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-16 16:00:16 +0900

    Suitcase basics, model and animation import, script and prefab

---
+++ Assets/Scripts/Interactables/Suitcase.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suitcase : GrabbableObject
+{
+    public GameObject[] keycardArray;
+
+    Animator suitcaseAnimator;
+    bool opened;
+
+    void Start()
+    {
+        suitcaseAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            OpenSuitCase();
+        }
+    }
+
+    public void OpenSuitCase()
+    {
+        suitcaseAnimator.Play("suitcase_open");
+
+        foreach (GameObject go in keycardArray)
+        {
+            go.SetActive(true);
+        }
+    }
+
+    public override void OnControllerEnter(PlayerViveController currentController, PlayerViveController.HandSource handSource)
+    {
+    }
+
+    public override void OnControllerExit()
+    {
+    }
+
+    public override void OnControllerStay()
+    {
+    }
+
+    public override void OnGrab()
+    {
+    }
+
+    public override void OnGrabReleased(bool snapped)
+    {
+    }
+
+    public override void OnUse()
+    {
+        if (!opened)
+        {
+            OpenSuitCase();
+            opened = true;
+        }
+    }
+}
