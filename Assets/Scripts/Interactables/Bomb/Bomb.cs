9c59aa57f49c7d46584b7335769d181517b00146
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-16 17:42:32 +0900

    Slight changes to suitcase, glassbox and bomb behaviours.

--- Assets/Scripts/Interactables/Bomb/Bomb.cs
+++ Assets/Scripts/Interactables/Bomb/Bomb.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour {
+public class Bomb : GrabbableObject
+{
+    private void Start()
+    {
+        enabled = false;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void OnControllerEnter(PlayerViveController currentController, PlayerViveController.HandSource handSource)
+    {
+    }
+
+    public override void OnControllerExit()
+    {
+    }
+
+    public override void OnControllerStay()
+    {
+    }
+
+    public override void OnGrab()
+    {
+    }
+
+    public override void OnGrabReleased(bool snapped)
+    {
+    }
+
+    public override void OnUse()
+    {
+    }
 }
