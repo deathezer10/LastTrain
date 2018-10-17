def2e53d4a5bd8f05f480b22cd6eaa6018be6fe0
Hui Sheng @ Laptop <deathezer10@gmail.com>
2018-10-17 17:25:54 +0900

    Initial death state screen fading

--- Assets/Scripts/Player/PlayerDeathHandler.cs
+++ Assets/Scripts/Player/PlayerDeathHandler.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using Valve.VR;
 
-public class PlayerDeathHandler : MonoBehaviour {
+public class PlayerDeathHandler : MonoBehaviour
+{
+
+    public Image m_FadeImage;
 
     public void KillPlayer()
     {
+        m_FadeImage.DOFade(1, 1).OnComplete(() =>
+        {
+            SceneManager.LoadScene("TrainStation");
+        });
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S) ||SteamVR_Input._default.inActions.GrabUse.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            KillPlayer();
+        }
     }
 
 }
