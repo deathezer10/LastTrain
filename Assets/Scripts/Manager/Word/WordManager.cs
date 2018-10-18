de69dbc4492bc35869795605dbf654c3708c3809
Kai <>
2018-10-18 15:15:38 +0900

    add localWord

--- Assets/Scripts/Manager/Word/WordManager.cs
+++ Assets/Scripts/Manager/Word/WordManager.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO; // ファイル書き込みに必要
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using Trident;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class WordManager : SingletonMonoBehaviour<WordManager> {
 	// application url
     const string URL = "https://script.google.com/macros/s/AKfycbzRSrUhjvQSuQBPbpXfV0kAukbZi5l7QFyxOklgMz_Htro5A6E/exec";
     private string _sheetName = "word";
-    private SerializeDictionary<string, string> _word;
+    private Dictionary<string,Dictionary<string, string>> _wordObjects = new Dictionary<string,Dictionary<string, string>>();
+    private Dictionary<string,string> _currentWord;
 
 	[SerializeField]
     private Language _currentLanguage = Language.En;
@@ -27,36 +33,132 @@ public class WordManager : SingletonMonoBehaviour<WordManager> {
 
     public IEnumerator Setup()
     {
-        var download = new WWW(URL + "?sheetName=" + _sheetName+"&language="+_currentLanguage.ToStringQuickly());
+        yield return StartCoroutine(LoadAllWords());
+
+        _currentWord = _wordObjects[_currentLanguage.ToStringQuickly()];
+
+        Debug.Log("complete.");
+
+        IsSetup = false;
+    }
+
+    private IEnumerator LoadAllWords()
+    {
+        foreach (Language language in Enum.GetValues(typeof(Language)))
+        {
+            var jsonString = string.Empty;
+
+            SerializeDictionary<string,string> json = null;
+
+            // TODO: Make Loading at the same time
+            yield return StartCoroutine(LoadWebJson(language, (_) =>
+            {
+                jsonString = _;
+                if(!string.IsNullOrEmpty(jsonString)){
+                    json = JsonToDic(jsonString);
+                    // 保存
+                    SaveJson(language, jsonString);
+                }
+            }));
+
+            if (string.IsNullOrEmpty(jsonString))
+            {
+                // 読み込み
+                yield return StartCoroutine(LoadLocalJson(language, (text) =>
+                {
+                    jsonString = text;
+                    json = JsonToDic(jsonString);
+                }));
+            }
+
+            // dicを設定
+            _wordObjects[language.ToStringQuickly()] = json;
+        }
+    }
+
+    private IEnumerator LoadWebJson(Language language, System.Action<string> callback)
+    {
+        var download = new WWW(URL + "?sheetName=" + _sheetName + "&language=" + language.ToStringQuickly());
         IsSetup = true;
         while (!download.isDone)
         {
             yield return new WaitForSeconds(0.1f);
         }
+
+        if (!string.IsNullOrEmpty(download.error))
+        {
+            Debug.LogError("download Error:" + download.error);
+        }
         Debug.Log(download.text);
 
-        var json = JsonUtility.FromJson<SerializeDictionary<string, string>>(download.text);
+        callback(download.text);
+    }
+
+    private SerializeDictionary<string, string> JsonToDic(string jsonString)
+    {
+        var json = JsonUtility.FromJson<SerializeDictionary<string, string>>(jsonString);
         if (json == null)
         {
-            Debug.LogError(download.text);
+            Debug.LogError(jsonString);
         }
         else
         {
             Debug.Log("Loading Complete.");
         }
-
-        _word = json;
-
-        IsSetup = false;
+        return json;
     }
 
-	public string GetWord(string key)
+    private void SaveJson(Language language,string text)
+    {
+        string path = "Resources/WordJson/" + language.ToStringQuickly()+".txt";
+
+        var fullPath = Application.dataPath + "/" + path;
+
+        StreamWriter writer;
+
+        if (!System.IO.File.Exists(fullPath))
+        {
+#if UNITY_EDITOR
+            // 存在しない場合作成
+            writer = File.CreateText(fullPath); 
+#else
+            Debug.LogError("text don't exist");
+            return;
+#endif
+        }else{
+            writer = new StreamWriter(fullPath, false); // 上書き
+        }
+        writer.WriteLine(text);
+        writer.Flush();
+        writer.Close();
+#if UNITY_EDITOR
+        AssetDatabase.Refresh();
+#endif
+    }
+
+    private IEnumerator LoadLocalJson(Language language ,Action<string> callback)
+    {
+        string path = "WordJson/" + language.ToStringQuickly();
+
+        var fullPath = Application.dataPath + "/" + path;
+        // Assetsフォルダからロード
+        var request = Resources.LoadAsync(path);  
+        yield return new WaitUntil(()=>!request.isDone);
+
+        var text = request.asset as TextAsset;
+
+        callback(text.text);
+    }
+
+    public string GetWord(string key)
 	{
-		string word = _word[key];
-		if(string.IsNullOrEmpty(word))
-		{
-			Debug.LogError("not setting word key");
-		}
+		string word = _currentWord[key];
+
+        if (string.IsNullOrEmpty(word))
+        {
+            Debug.LogError("not setting word key");
+            word = "not setting word";
+        }
 
 		return word;
 	}
---
+++ Assets/Scripts/Manager/Word/WordObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class WordObject : ScriptableObject
+{
+    public SerializeDictionary<string, string> _words;
+
+    public string GetWord(string key)
+    {
+        var word = _words[key];
+        if (string.IsNullOrEmpty(word))
+        {
+            Debug.LogError("not setting word key");
+            word = "not setting word";
+        }
+        return word;
+    }
+}
