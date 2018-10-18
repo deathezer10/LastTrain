3d0d0934e41708ae29acaadcf0c64911c356682b
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-18 11:37:36 +0900

    SawBlade changes, maybe vibration?

--- Assets/Scripts/Interactables/Bomb/BombWire.cs
+++ Assets/Scripts/Interactables/Bomb/BombWire.cs
@@ -17,27 +17,35 @@ public class BombWire : MonoBehaviour
             // Check if player is holding the glass and a piece hasn't just randomly landed on the wire collider
             if (controller != null && controller.gameObject.GetComponent<SteamVR_Behaviour_Pose>().GetVelocity().magnitude >= heldCutForce)
             {
-                GetComponent<BoxCollider>().enabled = false;
-
-                Bomb bomb = FindObjectOfType<Bomb>();
-
-                if (correctWire)
-                {
-                    Debug.Log("Bomb diffused, trigger next sequence.");
-                    bomb.CutRightWire();
-                    // Diffuse bomb - Display change in bomb state - Trigger next sequence
-                }
-                else
-                {
-                    Debug.Log("Bomb exploded, trigger death state.");
-                    bomb.CutWrongWire();
-                    // Explode - Death state
-                }
-
-                SwapWireModel();
+                WireCut();
             }
-            
         }
+        else if (other.tag == "SawBlade" && other.GetComponentInParent<SawBlade>().IsSpinning())
+        {
+            WireCut();
+        }
+    }
+
+    private void WireCut()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+
+        Bomb bomb = FindObjectOfType<Bomb>();
+
+        if (correctWire)
+        {
+            Debug.Log("Bomb diffused, trigger next sequence.");
+            bomb.CutRightWire();
+            // Diffuse bomb - Display a change in bomb state / play announcement about bomb diffusal?
+        }
+        else
+        {
+            Debug.Log("Bomb exploded, trigger death state.");
+            bomb.CutWrongWire();
+            // Explode - Death state
+        }
+
+        SwapWireModel();
     }
 
     private void SwapWireModel()
--- Assets/Scripts/Interactables/Concrete/SawBlade.cs
+++ Assets/Scripts/Interactables/Concrete/SawBlade.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Valve.VR;
 using UnityEngine;
 
 public class SawBlade : GrabbableObject
@@ -7,8 +8,23 @@ public class SawBlade : GrabbableObject
     public Color PH_SpinColor, PH_NotSpinColor;
     public GameObject PH_SpinIndicator;  // To be replaced with sound / animation for spin
 
-    bool spinning;
-    
+    string holdingControllerHand;
+    bool spinning, held;
+
+    private void Update()
+    {
+        if (spinning && held)
+        {
+            if (holdingControllerHand == SteamVR_Input_Sources.LeftHand.ToString())
+            {
+                SteamVR_Input.actionsVibration[0].Execute(0, 0.5f, 0.5f, 1, SteamVR_Input_Sources.LeftHand);
+            }
+            else
+            {
+                SteamVR_Input.actionsVibration[0].Execute(0, 0.5f, 0.5f, 1, SteamVR_Input_Sources.RightHand);
+            }
+        }
+    }
 
     public override void OnControllerEnter(PlayerViveController currentController, PlayerViveController.HandSource handSource)
     {
@@ -24,13 +40,18 @@ public class SawBlade : GrabbableObject
 
     public override void OnGrab()
     {
+        held = true;
+
+        holdingControllerHand = PlayerViveController.GetControllerThatHolds(gameObject).GetCurrentHand().ToString();
     }
 
     public override void OnGrabReleased()
     {
         spinning = false;
+        held = false;
 
         PH_SpinIndicator.GetComponent<MeshRenderer>().material.color = PH_NotSpinColor;
+        holdingControllerHand = "";
     }
 
     public override void OnUse()
