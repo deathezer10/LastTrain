bd9423ead09187ebf63e0bde87205017161641c1
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-18 10:38:30 +0900

    Added extinguisher model, adjusted toolbox size, added basic Sawblade.

---
+++ Assets/Scripts/Interactables/Concrete/SawBlade.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBlade : GrabbableObject
+{
+    public Color PH_SpinColor, PH_NotSpinColor;
+    public GameObject PH_SpinIndicator;  // To be replaced with sound / animation for spin
+
+    bool spinning;
+    
+
+    public override void OnControllerEnter(PlayerViveController currentController, PlayerViveController.HandSource handSource)
+    {
+    }
+
+    public override void OnControllerExit()
+    {
+    }
+
+    public override void OnControllerStay()
+    {
+    }
+
+    public override void OnGrab()
+    {
+    }
+
+    public override void OnGrabReleased()
+    {
+    }
+
+    public override void OnUse()
+    {
+        spinning = spinning ? false : true;
+
+        PH_SpinIndicator.GetComponent<MeshRenderer>().material.color = spinning ? PH_SpinColor : PH_NotSpinColor;
+    }
+
+    public bool IsSpinning()
+    {
+        return spinning;
+    }
+}
