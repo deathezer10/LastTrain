93a2e98357bf485961b071c2414715b35f80e4cc
DennisB <dennis1@hotmail.fi>
2018-10-24 12:06:49 +0900

    testing

---
+++ Assets/DoorIgnoreCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorIgnoreCollision : MonoBehaviour
+{
+
+    private BoxCollider Collider;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Collider = transform.gameObject.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Collider.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Collider.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        Collider.gameObject.GetComponent<Rigidbody>().Sleep();
+
+    }
+}
