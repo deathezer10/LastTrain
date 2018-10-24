7020c8d054ebabeaa3202b4b6631d5e3e13dfa9d
DennisB <dennis1@hotmail.fi>
2018-10-24 13:19:14 +0900

    test

--- Assets/DoorIgnoreCollision.cs
+++ Assets/DoorIgnoreCollision.cs
@@ -6,7 +6,7 @@ public class DoorIgnoreCollision : MonoBehaviour
 {
 
     private BoxCollider Collider;
-
+    private bool bIsColliding;
 
     // Use this for initialization
     void Start()
@@ -17,14 +17,27 @@ public class DoorIgnoreCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(bIsColliding)
+        {
+            Collider.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Collider.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            Collider.gameObject.GetComponent<Rigidbody>().Sleep();
+        }
     }
 
     void OnCollisionEnter(Collision other)
     {
+        print("Colission start");
+        bIsColliding = true;
         Collider.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         Collider.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         Collider.gameObject.GetComponent<Rigidbody>().Sleep();
 
     }
+
+    void OnCollisionLeave(Collision other)
+    {
+        bIsColliding = false;
+    }
+
 }
