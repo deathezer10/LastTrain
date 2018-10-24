83c63e9b651b7e135b0784ac8e22e9f9d5c9a206
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-24 11:53:28 +0900

    Scene fix, again?

--- Assets/Scripts/Interactables/Openables/UmbrCompartment.cs
+++ Assets/Scripts/Interactables/Openables/UmbrCompartment.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UmbrCompartment : MonoBehaviour
 {
+    public GameObject visualLockBottom, visualLockTop, physicsLockBottom, physicsLockTop, umbrella;
+
     bool rotating;
     Quaternion finalRotation = new Quaternion(0, 0.7f, 0, 0.7f);
     
@@ -20,8 +22,29 @@ public class UmbrCompartment : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "SawBlade" && other.GetComponentInParent<SawBlade>().IsSpinning())
+        {
+            OpenCompartment();
+            GetComponent<CapsuleCollider>().enabled = false;
+        }
+    }
+
     public void OpenCompartment()
     {
+        visualLockBottom.SetActive(false);
+        visualLockTop.SetActive(false);
+
+        physicsLockBottom.SetActive(true);
+        physicsLockBottom.transform.parent = null;
+
+        physicsLockTop.SetActive(true);
+        physicsLockTop.transform.parent = null;
+
+        umbrella.SetActive(true);
+        umbrella.transform.SetParent(null);
+
         rotating = true;
     }
 }
