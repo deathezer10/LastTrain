beaa2dec0bb25545d44cf7c1bae85f471e03ae2a
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-24 12:04:53 +0900

    Cats, umbrella compartment fix

---
+++ Assets/Scripts/Interactables/Openables/ExtghsrCompartment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtghsrCompartment : MonoBehaviour
+{
+    public GameObject visualExtinguisher, physicsExtinguisher;
+
+    bool rotating;
+    Quaternion finalRotation = new Quaternion(0, 0, 0, 0.7f);
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
