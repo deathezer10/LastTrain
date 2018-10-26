d7dd8aeb0b7f21f51b17ebd2fe3160da93f6268a
Kai <>
2018-10-26 16:39:34 +0900

    create

---
+++ Assets/Scripts/Utility/FPSSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSSetting : MonoBehaviour
+{
+
+    [SerializeField]
+    private int fps = 60;
+
+    [SerializeField]
+    private bool isDebug = false;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = fps;
+    }
+
+    private void Update()
+    {
+        if (!isDebug) return;
+        float fps = 1f / Time.deltaTime;
+        Debug.LogFormat("{0}fps", fps);
+    }
+}
