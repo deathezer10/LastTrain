355ef060243587caef0083e700c317dec5dae554
Hui Sheng @ Laptop <deathezer10@gmail.com>
2018-10-29 11:59:37 +0900

    revolver test

---
+++ Assets/Scripts/Interactables/Concrete/Revolver.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : GrabbableObject
+{
+
+    private GameObject m_LaserPointer;
+
+    private float m_PointerDistance;
+
+    Vector3 m_OriginalLocalPosition;
+    Vector3 m_OriginalScale;
+
+    bool m_IsGrabbing = false;
+
+    private void Start()
+    {
+        m_LaserPointer = transform.Find("LaserPointer").gameObject;
+        m_OriginalLocalPosition = m_LaserPointer.transform.localPosition;
+        m_OriginalScale = m_LaserPointer.transform.localScale;
+        m_PointerDistance = m_LaserPointer.transform.localScale.y;
+    }
+
+    public override void OnControllerEnter(PlayerViveController currentController)
+    {
+    }
+
+    public override void OnControllerExit()
+    {
+    }
+
+    public override void OnControllerStay()
+    {
+        if (m_IsGrabbing == false)
+            return;
+
+        RaycastHit hitInfo;
+        
+        if (Physics.Raycast(m_LaserPointer.transform.parent.position, m_LaserPointer.transform.forward, out hitInfo, m_PointerDistance))
+        {
+            m_LaserPointer.transform.position = Vector3.Lerp(hitInfo.point, m_LaserPointer.transform.parent.position, 0.5f);
+
+            Vector3 newScale = m_LaserPointer.transform.localScale;
+            newScale.z = m_LaserPointer.transform.localPosition.z * 2;
+            m_LaserPointer.transform.localScale = newScale;
+        }
+        else
+        {
+            m_LaserPointer.transform.localPosition = m_OriginalLocalPosition;
+            m_LaserPointer.transform.localScale = m_OriginalScale;
+        }
+    }
+
+    public override void OnGrab()
+    {
+        m_IsGrabbing = true;
+    }
+
+    public override void OnGrabReleased()
+    {
+        m_IsGrabbing = false;
+    }
+
+    public override void OnUse()
+    {
+    }
+
+    public override void OnUseDown()
+    {
+    }
+
+    public override void OnUseUp()
+    {
+    }
+}
