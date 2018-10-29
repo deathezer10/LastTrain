af387f6df23b12e0046866765a9a796ef2c80d36
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-29 14:08:02 +0900

    Lighter and Newspaper burning changes

---
+++ Assets/Scripts/Interactables/Concrete/NPBurnArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPBurnArea : MonoBehaviour
+{
+    ParticleSystem fireParticle, smokeParticle;
+
+    float burnThreshold = 45f;
+    float currentHeat;
+
+    void Start()
+    {
+        fireParticle = GetComponent<ParticleSystem>();
+        fireParticle.Stop();
+
+        smokeParticle = GetComponentInChildren<ParticleSystem>();
+        smokeParticle.Stop();
+    }
+
+    private void Burn()
+    {
+        fireParticle.Play();
+        smokeParticle.Play();
+    }
+
+    public void IncreaseHeat()
+    {
+        currentHeat++;
+
+        if (currentHeat >= burnThreshold)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            Burn();
+        }
+    }
+}
