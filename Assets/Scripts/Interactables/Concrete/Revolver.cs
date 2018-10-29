246223be897ecfb5e69c21f44b9c1b4b34f5b7e3
VR Testing Machine <deathezer10@gmail.com>
2018-10-29 12:11:34 +0900

    buggy af revolver

--- Assets/Scripts/Interactables/Concrete/Revolver.cs
+++ Assets/Scripts/Interactables/Concrete/Revolver.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Revolver : GrabbableObject
 {
 
-    private GameObject m_LaserPointer;
+    [SerializeField]
+    GameObject m_LaserPointer;
 
     private float m_PointerDistance;
 
@@ -16,7 +17,7 @@ public class Revolver : GrabbableObject
 
     private void Start()
     {
-        m_LaserPointer = transform.Find("LaserPointer").gameObject;
+        m_LaserPointer.SetActive(false);
         m_OriginalLocalPosition = m_LaserPointer.transform.localPosition;
         m_OriginalScale = m_LaserPointer.transform.localScale;
         m_PointerDistance = m_LaserPointer.transform.localScale.y;
@@ -35,14 +36,17 @@ public class Revolver : GrabbableObject
         if (m_IsGrabbing == false)
             return;
 
+
         RaycastHit hitInfo;
+
+        int mask = ~(1 << LayerMask.NameToLayer("Player"));
         
-        if (Physics.Raycast(m_LaserPointer.transform.parent.position, m_LaserPointer.transform.forward, out hitInfo, m_PointerDistance))
+        if (Physics.Raycast(m_LaserPointer.transform.parent.position, m_LaserPointer.transform.forward, out hitInfo, m_PointerDistance, mask))
         {
             m_LaserPointer.transform.position = Vector3.Lerp(hitInfo.point, m_LaserPointer.transform.parent.position, 0.5f);
 
             Vector3 newScale = m_LaserPointer.transform.localScale;
-            newScale.z = m_LaserPointer.transform.localPosition.z * 2;
+            newScale.y = m_LaserPointer.transform.localPosition.y * 2;
             m_LaserPointer.transform.localScale = newScale;
         }
         else
@@ -55,11 +59,13 @@ public class Revolver : GrabbableObject
     public override void OnGrab()
     {
         m_IsGrabbing = true;
+        m_LaserPointer.SetActive(true);
     }
 
     public override void OnGrabReleased()
     {
         m_IsGrabbing = false;
+        m_LaserPointer.SetActive(false);
     }
 
     public override void OnUse()
