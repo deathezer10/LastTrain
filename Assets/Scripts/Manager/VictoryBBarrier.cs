c5a28af5a37175cd29cd82759884010a780f58a5
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-31 15:15:34 +0900

    Wasting some time making Victory screen.

---
+++ Assets/Scripts/Manager/VictoryBBarrier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryBBarrier : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Balloon>() != null)
+        {
+            other.GetComponent<Balloon>().PopBalloon();
+        }
+    }
+}
