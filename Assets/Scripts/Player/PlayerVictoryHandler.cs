f19b81a70cb406712095bd6f6716afda676afc5e
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-31 14:10:53 +0900

    Basic victory scene addition

---
+++ Assets/Scripts/Player/PlayerVictoryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using Valve.VR;
+
+public class PlayerVictoryHandler : MonoBehaviour
+{
+
+    public ImageFader victoryFader;
+
+    public void PlayerVictory()
+    {
+        victoryFader.FadeIn(2, () =>
+        {
+            SceneManager.LoadScene("Victory");
+        });
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            PlayerVictory();
+        }
+    }
+
+}
