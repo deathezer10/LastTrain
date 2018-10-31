05fb39c261c28168d20d9874ae70fed6e4881287
Hui Sheng @ Laptop <deathezer10@gmail.com>
2018-10-31 11:23:04 +0900

    emergency door handle test

--- Assets/Scripts/Interactables/Concrete/EmergencyDoorHandle.cs
+++ Assets/Scripts/Interactables/Concrete/EmergencyDoorHandle.cs
@@ -4,4 +4,50 @@ using UnityEngine;
 
 public class EmergencyDoorHandle : GrabbableObject
 {
+    private PlayerViveController m_Controller;
+
+    private float m_LastZRot;
+
+    private bool m_Inserted = false;
+    public bool inserted {
+        get { return m_Inserted; }
+        set { m_Inserted = value; }
+    }
+
+    private bool m_Grabbing = false;
+
+    public override void OnControllerEnter(PlayerViveController currentController)
+    {
+        m_Controller = currentController;
+    }
+
+    public override void OnControllerStay()
+    {
+        if (m_Grabbing == false)
+            m_LastZRot = m_Controller.transform.localRotation.z;
+    }
+
+    public override void OnGrab()
+    {
+        m_Grabbing = true;
+    }
+
+    public override void OnGrabReleased()
+    {
+        m_Grabbing = false;
+    }
+
+    public override void OnGrabStay()
+    {
+        if (m_Controller != null)
+        {
+            float currentZRot = m_Controller.transform.localRotation.z;
+
+            if (m_LastZRot != currentZRot)
+            {
+                transform.Rotate(0, 0, currentZRot - m_LastZRot);
+            }
+        }
+    }
+
 }
