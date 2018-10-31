afdc70c6520b988c7a27a2f7e9b9fddb076f8c00
DennisB <dennis1@hotmail.fi>
2018-10-31 15:06:36 +0900

    Let's create a git push story, There was once a guy....

---
+++ Assets/TrainVelocity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainVelocity : MonoBehaviour {
+    private Vector3 previous;
+    private float velocity;
+    public float GetVelocity
+        {
+        get { return velocity; }
+        }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+        previous = transform.position;
+
+        print(velocity);
+    }
+}
