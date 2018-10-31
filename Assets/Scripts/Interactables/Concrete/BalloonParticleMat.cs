c81f328a9296fd86913f136601b86d73b19ae9e6
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-10-31 16:22:50 +0900

    Victory scene changes

---
+++ Assets/Scripts/Interactables/Concrete/BalloonParticleMat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonParticleMat : MonoBehaviour
+{
+    public void SetParticleColor(Color _matColor)
+    {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().material.color = _matColor;
+    }
+}
