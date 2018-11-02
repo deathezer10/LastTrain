251eb744feb4c94a0198952ada6424d9002a59e3
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-11-02 12:18:01 +0900

    StationDisplay changes, initial script for doors between cabins.

---
+++ Assets/Scripts/Train/TrainBetweenCabinDoors.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class TrainBetweenCabinDoors : MonoBehaviour
+{
+    float offset = -1.1f;
+    public GameObject[] betweenDoors;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            OpenBetweenDoors();
+        }
+    }
+
+    public void OpenBetweenDoors()
+    {
+        foreach (GameObject door in betweenDoors)
+        {
+            var tweener = door.transform.DOLocalMoveX(offset, 2).SetRelative();
+        }
+
+        StartCoroutine(OnDoorOpenFinish());
+    }
+
+    IEnumerator OnDoorOpenFinish()
+    {
+        yield return new WaitForSeconds(2.05f);
+        // Call for the next announcement
+        Debug.Log("Doors between the cabins are open.");
+    }
+}
