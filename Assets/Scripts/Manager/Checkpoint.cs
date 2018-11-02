3eea2ba5db64945dd1aba0951642bcf786a3452b
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-11-02 16:24:11 +0900

    Checkpoint fixes.

--- Assets/Scripts/Manager/Checkpoint.cs
+++ Assets/Scripts/Manager/Checkpoint.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Checkpoint : MonoBehaviour
 {
     public static bool CHECKPOINT_ACTIVATED;
 
     public static Checkpoint Instance { get; private set; } = null;
-
-    Vector3 playerResetPos = new Vector3(1f, 1.5f, 0f);
-    Vector3 playerResetRot = new Vector3(0f, -90f, 0f);
 
     void Awake()
     {
@@ -26,22 +22,9 @@ public class Checkpoint : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    private void Start()
-    {
-        if (SceneManager.GetActiveScene().name == "TrainStation")
-        {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-            if (CHECKPOINT_ACTIVATED)
-            {
-                player.transform.position = playerResetPos;
-                player.transform.eulerAngles = playerResetRot;
-            }
-        }
-    }
-
     private void Update()
     {
+        // Test PH
         if (Input.GetKeyDown(KeyCode.B))
         {
             CheckpointActivated();
---
+++ Assets/Scripts/Player/PlayerCheckpointReset.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerCheckpointReset : MonoBehaviour
+{
+    Vector3 playerResetPos = new Vector3(1f, 1.5f, 0f);
+    Vector3 playerResetRot = new Vector3(0f, -90f, 0f);
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "TrainStation")
+        {
+            if (Checkpoint.CHECKPOINT_ACTIVATED)
+            {
+                transform.position = playerResetPos;
+                transform.eulerAngles = playerResetRot;
+            }
+        }
+    }
+}
