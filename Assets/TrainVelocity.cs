5bc7c7200b495f35943c3b511d0eefe9926dd7b6
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-11-05 15:46:40 +0900

    Clean up.

--- Assets/TrainVelocity.cs
+++ Assets/TrainVelocity.cs
@@ -2,24 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrainVelocity : MonoBehaviour {
+public class TrainVelocity : MonoBehaviour
+{
     private Vector3 previous;
-    private float velocity;
-    public float GetVelocity
-        {
-        get { return velocity; }
-        }
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+    public float GetVelocity { get; private set; }
+
+    void Update()
+    {
+
+        GetVelocity = ((transform.position - previous).magnitude) / Time.deltaTime;
         previous = transform.position;
-
-        // print(velocity);
     }
 }
