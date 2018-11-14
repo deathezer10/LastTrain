483f65f5a26ebc00212b33904ea5a2b5ae6712de
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-11-14 16:19:45 +0900

    Banana prefab initial

--- Assets/Scripts/Interactables/Concrete/PartyGrenade.cs
+++ Assets/Scripts/Interactables/Concrete/PartyGrenade.cs
@@ -6,6 +6,8 @@ public class PartyGrenade : GrabbableObject, IShootable
 {
     GameObject grenadeParticlePrefab;
 
+    PlayerViveController m_CurrentController;
+
     private void Explode()
     {
         // Boom?
@@ -22,9 +24,26 @@ public class PartyGrenade : GrabbableObject, IShootable
         Explode();
     }
 
+    public override bool hideControllerOnGrab { get { return true; } }
+
+    public override void OnControllerEnter(PlayerViveController currentController)
+    {
+        base.OnControllerEnter(currentController);
+        m_CurrentController = currentController;
+    }
+
+    public override void OnControllerExit()
+    {
+        base.OnControllerExit();
+    }
+
     public override void OnGrab()
     {
         base.OnGrab();
+
+        transform.rotation = m_CurrentController.transform.rotation;
+        transform.Rotate(new Vector3(0, -90, 0));
+        transform.position = m_CurrentController.transform.position;
     }
 
     public override void OnUse()
