7d17cf917fb94ba0227eb803da50964cb5fd8224
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-11-14 13:38:25 +0900

    Grenade prefab initial

---
+++ Assets/Scripts/Interactables/Concrete/PartyGrenade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyGrenade : GrabbableObject, IShootable
+{
+    GameObject grenadeParticlePrefab;
+
+    private void Explode()
+    {
+        // Boom?
+    }
+
+    private void PullPin()
+    {
+        StartCoroutine(PinPullTimer());
+    }
+
+    private IEnumerator PinPullTimer()
+    {
+        yield return new WaitForSeconds(2f);
+        Explode();
+    }
+
+    public override void OnGrab()
+    {
+        base.OnGrab();
+    }
+
+    public override void OnUse()
+    {
+        PullPin();
+    }
+
+    public void OnShot(Revolver revolver)
+    {
+        Explode();
+    }
+}
