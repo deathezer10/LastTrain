042c91b95facc5153480a286d000611bb14366ad
DennisB <dennis1@hotmail.fi>
2018-11-15 11:27:36 +0900

    +

--- Assets/SpeedMeter.cs
+++ Assets/SpeedMeter.cs
@@ -96,17 +96,9 @@ public class SpeedMeter : MonoBehaviour
 
         else
         {
-            if(previousSpeed < stationMover.currentSpeed)
-            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, Mathf.Lerp(DefaultRotation, MaxRotation,normalize01(stationMover.currentSpeed,0,4)), transform.localRotation.eulerAngles.z);
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, Mathf.Lerp(DefaultRotation, MaxRotation,normalize01(stationMover.currentSpeed,0,20) *3), transform.localRotation.eulerAngles.z);
 
-            if(previousSpeed > stationMover.currentSpeed)
-                transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, Mathf.Lerp(DefaultRotation, MaxRotation, normalize01(stationMover.currentSpeed, 0, 20)), transform.localRotation.eulerAngles.z);
-
-            if(previousSpeed == stationMover.currentSpeed)
-            {
-                transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, Mathf.Lerp(DefaultRotation, MaxRotation, normalize01(stationMover.currentSpeed, 0, 4)), transform.localRotation.eulerAngles.z);
-            }
-
+             
             if (bLeftSwivel)
                 transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y - 0.5f, transform.localRotation.eulerAngles.z);
 
