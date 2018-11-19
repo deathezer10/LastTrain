7329735aca1043eff21fef6f391d7943041a32f6
VR Testing Machine <deathezer10@gmail.com>
2018-11-19 13:15:31 +0900

    Doll changes

--- Assets/Scripts/Interactables/Concrete/Doll.cs
+++ Assets/Scripts/Interactables/Concrete/Doll.cs
@@ -12,33 +12,23 @@ public class Doll : GrabbableObject, IShootable
 
     Transform playerHeadTrans;
 
+    Vector3 headInitRot;
+
     float flashEndTime;
     bool playerWithinRange;
-
-    Vector3 newHeadEuler = new Vector3();
-    Vector3 unclampedHead = new Vector3();
 
     void Start()
     {
         playerHeadTrans = GameObject.FindGameObjectWithTag("MainCamera").transform;
         dollEyeMat.SetColor("_EmissionColor", initialColor);
+        headInitRot = head.localEulerAngles;
     }
 
     private void Update()
     {
         if (playerWithinRange)
         {
-            // Clamp this to a reasonable range based on body transform rotations
             head.LookAt(playerHeadTrans);
-
-            unclampedHead = head.localEulerAngles;
-
-            newHeadEuler.x = Mathf.Clamp(unclampedHead.x, -55, 45);
-            newHeadEuler.y = Mathf.Clamp(unclampedHead.y, -85, 85);
-            newHeadEuler.z = Mathf.Clamp(unclampedHead.z, -40, 40);
-
-            // Debug.Log("Unclamped euler: " + unclampedHead + " Clamped euler: " + newHeadEuler);
-            head.localEulerAngles = newHeadEuler;
         }
 
         // Debug
@@ -74,13 +64,20 @@ public class Doll : GrabbableObject, IShootable
                 dollEyeMat.SetColor("_EmissionColor", initialColor);
                 flash = false;
             }
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.2f);
         }
+
+        dollEyeMat.SetColor("_EmissionColor", initialColor);
     }
 
     public void PlayerWithinRange(bool _inRange)
     {
         playerWithinRange = _inRange;
+
+        if (!_inRange)
+        {
+            head.DOLocalRotate(headInitRot, 0.4f, RotateMode.Fast);
+        }
     }
 
     public void OnShot(Revolver revolver)
