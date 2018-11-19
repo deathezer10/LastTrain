038adcbbdff225f89276a5a6f4426fa4793e4c5c
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-11-19 11:35:21 +0900

    Initial Doll stuff

---
+++ Assets/Scripts/Interactables/Concrete/DollRange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollRange : MonoBehaviour
+{
+    Doll dollScript;
+
+    private void Start()
+    {
+        dollScript = GetComponentInParent<Doll>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dollScript.PlayerWithinRange(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            dollScript.PlayerWithinRange(false);
+        }
+    }
+}
