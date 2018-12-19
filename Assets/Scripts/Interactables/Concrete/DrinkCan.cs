4c3263312cf67cc0823a710d89c18661a7bebfde
TVaajamaki_Laptop <tvaajamaki@gmail.com>
2018-12-19 13:49:27 +0900

    Drink can

--- Assets/Scripts/Interactables/Concrete/DrinkCan.cs
+++ Assets/Scripts/Interactables/Concrete/DrinkCan.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DrinkCan : GrabbableObject
 {
+    [SerializeField]
+    Transform m_OpenerTransform;
 
+    [SerializeField]
+    MeshRenderer m_CoverRenderer;
+
+    [SerializeField]
+    Vector3 m_OpenerEndRot;
+
+    bool m_bOpened;
+
+    private void Start()
+    {
+        GetComponent<ParticleSystem>().Stop();
+    }
 
     public override void OnGrab()
     {
@@ -14,5 +29,19 @@ public class DrinkCan : GrabbableObject
     public override void OnUse()
     {
         base.OnUse();
+
+        if (!m_bOpened)
+        {
+            m_bOpened = true;
+
+            m_OpenerTransform.DOLocalRotate(m_OpenerEndRot, 0.1f, RotateMode.Fast);
+            m_CoverRenderer.enabled = false;
+
+            GetComponent<AudioPlayer>().Play();
+
+            GetComponent<ParticleSystem>().Play();
+
+            GetComponentInChildren<AudioPlayer>().Play();
+        }
     }
 }
