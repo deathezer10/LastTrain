2a9b0855e3b15952a83654e78979c8b3bc35f9ad
negidevelop <negidevelop@gmail.com>
2019-01-30 17:33:06 +0900

    ポスターぶら下げ　他修正

---
+++ Assets/Scripts/Interactables/Concrete/Poster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class Poster : StationaryObject
+{
+
+    private void Start()
+    {
+
+        var startRotate = this.transform.localEulerAngles;
+        Debug.Log($"Rot : {startRotate}");
+        Sequence sequence = DOTween.Sequence()
+            .Append(this.transform.DORotate(startRotate + new Vector3(0f, 0f, 15.0f), 3f).SetEase(Ease.OutSine))
+            .Append(this.transform.DORotate(startRotate, 3f).SetEase(Ease.OutSine));
+
+        sequence.Play().SetLoops(-1, LoopType.Restart);
+    }
+}
