16a15099af7479804860644ad62a3a8f876e01d3
negidevelop <negidevelop@gmail.com>
2019-02-01 14:38:27 +0900

    アナウンス修正

--- Assets/Scripts/Manager/TimeLeftAnnouncements.cs
+++ Assets/Scripts/Manager/TimeLeftAnnouncements.cs
@@ -19,6 +19,9 @@ public class TimeLeftAnnouncements : MonoBehaviour
         StartCoroutine(TimeLeftRoutine());
     }
 
+    [SerializeField]
+    private StationMover _mover;
+
     private IEnumerator TimeLeftRoutine()
     {
         yield return new WaitForSeconds(totalTrainTime - 180f);
@@ -27,15 +30,30 @@ public class TimeLeftAnnouncements : MonoBehaviour
         AnnouncementManager.Instance.PlayAnnouncement3D("threeMinutes_remain", AnnouncementManager.AnnounceType.Queue, 0f);
         yield return new WaitForSeconds(90f);
 
+        if (!isAnnouncement()) yield break;
+
         AnnouncementManager.Instance.PlayAnnouncement3D("announcement_chime", AnnouncementManager.AnnounceType.Queue, 0f);
         AnnouncementManager.Instance.PlayAnnouncement3D("ninetySeconds_remain", AnnouncementManager.AnnounceType.Queue, 0f);
         yield return new WaitForSeconds(60f);
+
+        if (!isAnnouncement()) yield break;
 
         AnnouncementManager.Instance.PlayAnnouncement3D("announcement_chime", AnnouncementManager.AnnounceType.Queue, 0f);
         AnnouncementManager.Instance.PlayAnnouncement3D("thirtySeconds_remain", AnnouncementManager.AnnounceType.Queue, 0f);
         yield return new WaitForSeconds(20f);
 
+        if (!isAnnouncement()) yield break;
+
         AnnouncementManager.Instance.PlayAnnouncement3D("announcement_chime", AnnouncementManager.AnnounceType.Queue, 0f);
         AnnouncementManager.Instance.PlayAnnouncement3D("tenSeconds_remain", AnnouncementManager.AnnounceType.Queue, 0f);
+    }
+
+    private bool isAnnouncement()
+    {
+        if (_mover.currentSpeed == 0)
+        {
+            return false;
+        }
+        return true;
     }
 }
